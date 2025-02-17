9c5dbebf1f37d42142e0f31ac76edc6700e37217
효석 이 <lhs19960330@gmail.com>
2025-02-17 15:35:18 +0900

    초기 커밋.

---
+++ BaekjoonAlgorithm/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace BaekjoonAlgorithm
+{
+
+    internal class Program
+    {
+        static void Main( string [] args )
+        {
+            string n;
+            string p;
+            n = Console.ReadLine();
+            p = Console.ReadLine();
+            int N;
+            if ( n == null )
+                return;
+            if ( p == null )
+                return;
+            N = int.Parse(n);
+            int [] P = p.Split().Select(int.Parse).ToArray();
+            Array.Sort(P);
+
+            int sum = 0;
+            int total = 0;
+            for ( int i = 0; i < N; i++ )
+            {
+                sum += P [i];
+                total += sum;
+            }
+            Console.WriteLine(total);
+        }
+    }
+}
