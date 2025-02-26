521e42ebc52a045dd897b10a1dc75585fd4783fc
이효석 <lhs19960330@gmail.com>
2025-02-26 16:25:25 +0900

    feat : 11724, (11725 에러)

---
+++ 11724/Program.cs
@@ -0,0 +1,53 @@
+﻿namespace _11724
+{
+    internal class Program
+    {
+        //https://www.acmicpc.net/problem/11724
+        static bool [] visited;
+        static Queue<int> queue;
+        static List<int> [] graph;
+        static void DFS( int Node )
+        {
+            visited [Node] = true;
+
+            foreach ( var next in graph [Node] )
+            {
+                if ( !visited [next] )
+                {
+                    DFS(next);
+                }
+            }
+        }
+        static void Main( string [] args )
+        {
+            int N; int M;
+
+            int [] number = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            N = number [0];// 정점의 갯수 
+            M = number [1];// 간선의 갯수
+            graph = new List<int> [N + 1];
+            for ( int i = 1; i <= N; i++ )
+                graph [i] = new List<int>();
+            for ( int i = 0; i < M; i++ )
+            {
+                int [] c = Console.ReadLine().Split().Select(int.Parse).ToArray();
+                int u = c [0];
+                int v = c [1];
+                graph [u].Add(v);
+                graph [v].Add(u);
+            }
+            visited = new bool [N + 1];
+
+            int count = 0;
+            for ( int i = 1; i <= N; i++ )
+            {
+                if( !visited [i] )
+                {
+                    DFS (i);
+                    count++;
+                }
+            }
+            Console.WriteLine(count);
+        }
+    }
+}
---
+++ 11725/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Xml;
+
+namespace _11725
+{
+    internal class Program
+    {
+        //https://www.acmicpc.net/problem/11725
+        static List<int> [] tree;
+        static int [] parent;
+        static bool [] visited;
+
+        static int DFS( int node )
+        {
+            visited [node] = true;
+
+            foreach ( int next in tree [node] )
+            {
+                if ( parent [next] == 0 )
+                {
+                    parent [next] = node; // 부모 기록
+                    return next;
+                }
+            }
+            return 0;
+        }
+
+        static void Main( string [] args )
+        {
+            int N = int.Parse(Console.ReadLine());
+            tree = new List<int> [N + 1]; // 1-based index 사용
+            parent = new int [N + 1];
+            visited = new bool [N + 1];
+
+            for ( int i = 1; i <= N; i++ )
+                tree [i] = new List<int>();
+
+            for ( int i = 1; i < N; i++ )
+            {
+                int [] input = Console.ReadLine().Split().Select(int.Parse).ToArray();
+                int a = input [0];
+                int b = input [1];
+
+                tree [a].Add(b);
+                tree [b].Add(a);
+            }
+
+
+            for ( int i = 2; i <= N; i++ )
+            {
+                Console.WriteLine(DFS(i)); // 2번 노드부터 부모 출력
+            }
+        }
+    }
+}
