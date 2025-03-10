ee19fe520b43d268bcf50dded915c6f89b15fcb9
효석 이 <lhs19960330@gmail.com>
2025-03-10 17:40:40 +0900

    feat : 25206

---
+++ 25206/Program.cs
@@ -0,0 +1,42 @@
+﻿namespace _25206
+{
+    internal class Program
+    {
+        // https://www.acmicpc.net/problem/25206
+        static void Main( string [] args )
+        {
+            Dictionary<string, float> grades = new Dictionary<string, float>();
+            float [] subjectGrade = new float [20];
+            grades.Add("A+", 4.5f);
+            grades.Add("A0", 4f);
+            grades.Add("B+", 3.5f);
+            grades.Add("B0", 3f);
+            grades.Add("C+", 2.5f);
+            grades.Add("C0", 2f);
+            grades.Add("D+", 1.5f);
+            grades.Add("D0", 1f);
+            grades.Add("F", 0);
+
+            float totalCredits = 0;
+            float weightedGradeSum = 0;
+
+            for ( int i = 0; i < 20; i++ )
+            {
+                string [] input = Console.ReadLine().Split();
+                float credit = float.Parse(input [1]);
+                string grade = input [2];
+
+                // P 과목은 제외
+                if ( grade != "P" )
+                {
+                    weightedGradeSum += credit * grades [grade];
+                    totalCredits += credit;
+                }
+            }
+
+            // 전공 평점 계산
+            float majorGPA = weightedGradeSum / totalCredits;
+            Console.WriteLine($"{majorGPA:F4}");
+        }
+    }
+}
