d85cae7ea70abd1bbb30e927ad681b59b48eb99c
효석 이 <lhs19960330@gmail.com>
2025-03-11 15:50:18 +0900

    feat : 2563

---
+++ 2563/Program.cs
@@ -0,0 +1,38 @@
+﻿namespace _2563
+{
+    internal class Program
+    {
+        //https://www.acmicpc.net/problem/2563
+        static void Main( string [] args )
+        {
+            int N = int.Parse(Console.ReadLine());
+            // 전체 크기
+            bool [,] paper = new bool [100, 100];
+            for ( int i = 0; i < N; i++ )
+            {
+                string [] input = Console.ReadLine().Split();
+                int x = int.Parse(input [0]);
+                int y = int.Parse(input [1]);
+                // 입력받은 x와 y에 true로 만들어서 크기 결정
+                for ( int j = x; j < x + 10; j++ )
+                {
+                    for(int k = y; k < y + 10; k++ )
+                    {
+                        paper [j,k] = true;
+                    }
+                }
+            }
+            int area = 0;
+            //true인곳만 계산해서 크기 만듬
+            for ( int i = 0; i < 100; i++ )
+            {
+                for ( int j = 0; j < 100; j++ )
+                {
+                    if ( paper [i, j] ) area++;
+                }
+            }
+            Console.WriteLine(area);
+
+        }
+    }
+}
--- Test/Program.cs
+++ Test/Program.cs
@@ -7,6 +7,26 @@ namespace Test
     {
         static void Main( string [] args )
         {
+
+            string [] input = new string [5];
+            for ( int i = 0; i < 5; i++ )
+            {
+                input [i] = Console.ReadLine();
+            }
+            string result = "";
+            for ( int i = 0; i < 15; i++ )
+            {
+                for ( int j = 0; j < 5; j++ ) 
+                {
+                    if ( i < input[j].Length )
+                    {
+                        result += input[j] [i];
+                    }
+                }
+            }
+
+            Console.WriteLine(result);
+
         }
     }
 }
