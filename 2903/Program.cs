d3140b2f7afda4c59ba52b5eceb450ab4892e9ed
효석 이 <lhs19960330@gmail.com>
2025-03-12 16:44:43 +0900

    feat : 2903

---
+++ 2903/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace _2903
+{
+    internal class Program
+    {
+        //https://www.acmicpc.net/problem/2903
+        // 초기 상태는 점 4개인상태       변 1*1
+        // 1번 이동후면 한 변당 3개 점 즉 변 3*3
+        // 2번 이동후면 한 변당 5개 점 즉 변 5*5
+        // 3번 이동후면 한 변당 9개 점 즉 변 9*9
+        // 한 변당 점의 갯수는 (전 한 변당 점의 갯수)*2-1
+        // 총 점의 갯수는 한변의 점의 갯수 제곱
+        static void Main( string [] args )
+        {
+            int N = int.Parse(Console.ReadLine());
+            // 초기 한변의 점 갯수
+            int L = 2;
+            
+            for(int i= 0; i < N; i++)
+            {
+                L = L * 2 - 1;
+            }
+            Console.WriteLine( L *L);
+        }
+
+    }
+}
