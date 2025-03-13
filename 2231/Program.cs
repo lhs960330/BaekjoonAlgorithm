d453166fd54117c7db45ff9d31ba1b0e3bc05f20
효석 이 <lhs19960330@gmail.com>
2025-03-13 19:36:09 +0900

    feat : 2231

---
+++ 2231/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace _2231
+{
+    internal class Program
+    {
+        //https://www.acmicpc.net/problem/2231
+
+        static void Main( string [] args )
+        {
+            int N = int.Parse(Console.ReadLine());
+            int answer = 0;
+            for ( int i = 0; i < N; i++ )
+            {
+                int a = i;
+                string b = i.ToString();
+
+                for ( int j = 0; j < b.Length; j++ )
+                {
+                    a += b [j] - '0';
+                }
+                if ( N == a )
+                {
+                    answer = i;
+                    break;
+                }
+
+            }
+            if ( answer == 0 )
+                Console.WriteLine(0);
+            else
+                Console.WriteLine(answer);
+        }
+    }
+}
