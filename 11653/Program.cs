e02ef11d988a6e2442ea07ed45b81a468283ad72
효석 이 <lhs19960330@gmail.com>
2025-03-13 17:51:46 +0900

    feat : 11653

---
+++ 11653/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace _11653
+{
+    internal class Program
+    {
+        // https://www.acmicpc.net/problem/11653
+
+        static void Main( string [] args )
+        {
+            int N = int.Parse(Console.ReadLine());
+            for ( int i = 2; i * i <= N; i++ )
+            {
+                while ( N % i == 0 ) // i로 나누어 떨어지는 동안 반복
+                {
+                    Console.WriteLine(i);
+                    N /= i; // N을 i로 나누어 갱신
+                }
+            }
+            if ( N > 1 )
+                Console.WriteLine(N);
+        }
+
+    }
+}
