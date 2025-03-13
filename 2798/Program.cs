aa28cf9d50dfe97ee7ff3cc8d651bdcbc374bdbc
효석 이 <lhs19960330@gmail.com>
2025-03-13 18:50:49 +0900

    feat : 2798

---
+++ 2798/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace _2798
+{
+    internal class Program
+    {
+        //https://www.acmicpc.net/problem/2798
+        static void Main( string [] args )
+        {
+            int [] input = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            int N = input [0];
+            int M = input [1];
+            int [] num = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+            int current = 0;
+            for ( int i = 0; i < N; i++ )
+            {
+                int sum = 0;
+                for ( int j = i+1; j < N; j++ )
+                {
+                    for ( int k = j+1; k < N; k++ )
+                    {
+                        sum = num [i] + num [j] + num [k];
+                        if ( M >= sum && current < sum )
+                            current = sum;
+
+                    }
+                }
+            }
+            Console.WriteLine(current);
+        }
+    }
+}
--- Test/Program.cs
+++ Test/Program.cs
@@ -1,27 +1,18 @@
-﻿using System;
-using System.Linq;
-
-namespace PerfectNumber
+﻿namespace PerfectNumber
 {
     internal class Program
     {
-        static bool IsPrime( int num )
-        {
-            if ( num < 2 ) return false;
-            for ( int j = 2; j * j <= num; j++ )
-            {
-                if ( num % j == 0 ) return false; 
-            }
-            return true;
-        }
         static void Main( string [] args )
         {
-            int N = int.Parse(Console.ReadLine());
-            int [] num = Console.ReadLine().Split().Select(int.Parse).ToArray();
-            int count = num.Count(IsPrime);
-
-            Console.Write(count);
-
+            decimal n  = decimal.Parse(Console.ReadLine());
+            
+            Console.WriteLine(n * n);
+            if ( n * n > int.MaxValue )
+                Console.WriteLine(4);
+            else if( n  == 1)
+                Console.WriteLine(1);
+            else
+                Console.WriteLine(2);
         }
     }
 }
