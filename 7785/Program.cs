9b6a61ce09ea19f62ffcd291d4ff33d0c16eeb84
효석 이 <lhs19960330@gmail.com>
2025-03-20 13:43:57 +0900

    feat : 7785

---
+++ 7785/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace _7785
+{
+    internal class Program
+    {
+        // https://www.acmicpc.net/problem/7785
+        static void Main( string [] args )
+        {
+            int n = int.Parse( Console.ReadLine() );
+            Dictionary<string, string> entryLog = new Dictionary<string, string>();
+            for ( int i = 0; i < n; i++ )
+            {
+                string [] input = Console.ReadLine().Split().ToArray();
+                if ( entryLog.ContainsKey(input [0]) )
+                {
+                    entryLog [input [0]] = input [1];
+                }
+                else
+                {
+                    entryLog.Add(input [0], input [1]);
+                }
+            }
+            foreach ( string key in entryLog.Keys )
+            {
+                Console.WriteLine( key );
+            }
+        }
+    }
+}
