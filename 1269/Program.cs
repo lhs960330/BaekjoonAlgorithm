b5e18c6d8f9be9b456b3ce0f317c20f21c6f56b4
효석 이 <lhs19960330@gmail.com>
2025-03-20 15:07:58 +0900

    feat : 1269

---
+++ 1269/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace _1269
+{
+    internal class Program
+    {
+        //https://www.acmicpc.net/problem/1269
+        static void Main( string [] args )
+        {
+            int [] input = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            int N = input [0];
+            int M = input [1];
+            int [] input1 = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            HashSet<int> A = new HashSet<int>(N);
+            HashSet<int> AB = new HashSet<int>(N);
+            for ( int i = 0; i < N; i++ )
+            {
+                A.Add(input1 [i]);
+                AB.Add(input1 [i]);
+            }
+            int [] input2 = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            HashSet<int> B = new HashSet<int>(M);
+            HashSet<int> BA = new HashSet<int>(M);
+            for ( int i = 0; i < M; i++ )
+            {
+                B.Add(input2 [i]);
+                BA.Add(input2 [i]);
+            }
+            AB.ExceptWith(B);
+            BA.ExceptWith(A);
+            Console.WriteLine($"{AB.Count + BA.Count}");
+        }
+    }
+}
