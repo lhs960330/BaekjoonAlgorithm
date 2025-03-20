a68f73391947f9757ecf92f6d323f116aa35c16e
효석 이 <lhs19960330@gmail.com>
2025-03-20 15:44:06 +0900

    11478

---
+++ 11478/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace _11478
+{
+    internal class Program
+    {
+        // https://www.acmicpc.net/problem/11478
+        static void Main( string [] args )
+        {
+            string S = Console.ReadLine();
+            HashSet<string> answer = new HashSet<string>(); // 해시셋을 통해 같은 요소 없애기
+
+            for ( int i = 0; i < S.Length; i++ )
+            {                
+                for ( int j = i + 1; j <= S.Length; j++ )
+                {
+                    // 예시) S를 i(0) ~ j-i(2-0) 까지(0번째부터 2번째까지) 짜른 문자열을 answer에 넣기
+                    answer.Add(S.Substring(i, j - i));
+                }
+            }
+            Console.WriteLine( answer.Count );
+        }
+    }
+}
