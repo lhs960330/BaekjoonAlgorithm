88718dfae17a59f48c74076b6b1a48493358b9c0
효석 이 <lhs19960330@gmail.com>
2025-03-25 18:31:38 +0900

    11866 28279

---
+++ 11866/Program.cs
@@ -0,0 +1,43 @@
+﻿namespace _11866
+{
+    internal class Program
+    {
+        //https://www.acmicpc.net/problem/11866
+        static void Main( string [] args )
+        {
+            int [] input = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+            int N = input [0];
+            int K = input [1];
+            Queue<int> queue = new Queue<int>();
+            StringWriter sw = new StringWriter();
+            for ( int i = 1; i <= N; i++ )
+            {
+                queue.Enqueue(i);
+            }
+            int count = 1;
+            Queue<int> answer = new Queue<int>();
+            while ( queue.Count > 0 )
+            {
+                if ( count != K )
+                {
+                    count++;
+                    queue.Enqueue(queue.Dequeue());
+                }
+                else
+                {
+                    count = 1;
+                    answer.Enqueue(queue.Dequeue());
+                }
+            }
+            Console.Write("<");
+            for(int i = 0; i < N; i++ ) 
+            {
+                if ( answer.Count > 1 )
+                    Console.Write($"{answer.Dequeue()}, ");
+                else
+                    Console.Write($"{answer.Dequeue()}>");
+            }
+        }
+    }
+}
---
+++ 28279/Program.cs
@@ -0,0 +1,71 @@
+﻿namespace _28279
+{
+    internal class Program
+    {
+        // https://www.acmicpc.net/problem/28279
+        static void Main( string [] args )
+        {
+            LinkedList<int> ints = new LinkedList<int>();
+            int N = int.Parse(Console.ReadLine());
+            StringWriter sw = new StringWriter();
+            for ( int i = 0; i < N; i++ )
+            {
+                int [] command = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+                switch ( command [0] )
+                {
+                    case 1:
+                        ints.AddFirst(command [1]);
+                        break;
+                    case 2:
+                        ints.AddLast(command [1]);
+                        break;
+                    case 3:
+                        if ( ints.Count > 0 )
+                        {
+                            sw.WriteLine($"{ints.First.Value}");
+                            ints.RemoveFirst();
+                        }
+                        else
+                            sw.WriteLine("-1");
+                        break;
+                    case 4:
+                        if ( ints.Count > 0 )
+                        {
+                            sw.WriteLine($"{ints.Last.Value}");
+                            ints.RemoveLast();
+                        }
+                        else
+                            sw.WriteLine("-1");
+                        break;
+                    case 5:
+                        sw.WriteLine($"{ints.Count}");
+                        break;
+                    case 6:
+                        if ( ints.Count > 0 )
+                            sw.WriteLine(0);
+                        else
+                            sw.WriteLine(1);
+                        break;
+                    case 7:
+                        if ( ints.Count > 0 )
+                        {
+                            sw.WriteLine($"{ints.First.Value}");
+                        }
+                        else
+                            sw.WriteLine("-1");
+                        break;
+                    case 8:
+                        if ( ints.Count > 0 )
+                        {
+                            sw.WriteLine($"{ints.Last.Value}");
+                        }
+                        else
+                            sw.WriteLine("-1");
+                        break;
+                }
+            }
+            Console.Write(sw.ToString());
+        }
+    }
+}
