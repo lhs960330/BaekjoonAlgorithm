4f2a33bd7858f8e3ec96db98e5e88c5257c3ddea
효석 이 <lhs19960330@gmail.com>
2025-03-28 15:35:01 +0900

    25192

---
+++ 25192/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace _25192
+{
+    internal class Program
+    {
+        //https://www.acmicpc.net/problem/25192
+        static void Main( string [] args )
+        {
+            int N = int.Parse(Console.ReadLine());
+            HashSet<string> nameList = new HashSet<string>();
+            int answer = 0;
+            for ( int i = 0; i < N; i++ )
+            {
+                string input = Console.ReadLine();
+                // 새로운 사람이 들어오면 리스트 초기화
+                if ( input == "ENTER" )
+                    nameList.Clear();
+
+                // 리스트에 없다면 아직 인사하지 않은 사람
+                if ( !nameList.Contains(input) )
+                {
+                    // ENTER는 이름이 아니니 걸러서 리스트에 넣어줌
+                    if ( input != "ENTER" )
+                    {
+                        answer++;
+                        nameList.Add(input);
+                    }
+                }
+
+            }
+            Console.WriteLine(answer);
+        }
+    }
+}
--- Test/Program.cs
+++ Test/Program.cs
@@ -1,36 +1,16 @@
-﻿using System.Reflection.PortableExecutable;
-
-namespace PerfectNumber
+﻿namespace PerfectNumber
 {
     internal class Program
     {
-        
         static void Main( string [] args )
         {
-            int T = int.Parse(Console.ReadLine());
-            int [] A = new int [T];
-            int [] B = new int [T];
-            int [] lcm = new int [T];
-            for ( int i = 0; i < T; i++ )
-            {
-                int [] input = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            int N = int.Parse(Console.ReadLine());
+            int [] input = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            int answer = 0;
+            Array.Sort(input);
 
-                A [i] = input [0];
-                B [i] = input [1];
-                lcm [i] = ( A [i] * B [i] ) / GCD(A [i], B [i]);
-            }
-            foreach(int i in lcm )
-                Console.WriteLine(i);
-        }
-        static int GCD( int a, int b )
-        {
-            while ( b != 0 )
-            {
-                int temp = b;
-                b = a % b;
-                a = temp;
-            }
-            return a;
+            answer = input [0] * input [input.Length - 1];
+            Console.WriteLine(answer);
         }
     }
 }
