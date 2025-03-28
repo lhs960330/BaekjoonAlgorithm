544260b2457d7935c644b26de04af609fd401c3c
효석 이 <lhs19960330@gmail.com>
2025-03-28 15:04:23 +0900

    1010

---
+++ 1010/Program.cs
@@ -0,0 +1,28 @@
+﻿using System.Text;
+
+namespace _1010
+{
+    internal class Program
+    {
+        // https://www.acmicpc.net/problem/1010
+        static void Main( string [] args )
+        {
+            int T = int.Parse(Console.ReadLine());
+            StringBuilder sb = new StringBuilder();
+            for ( int i = 0; i < T; i++ )
+            {
+                int [] input = Console.ReadLine().Split().Select(int.Parse).ToArray();
+                int N = input [0];
+                int M = input [1];
+                long answer = 1;
+                for ( int j = 0; j < N; j++ )
+                {
+                    answer *= ( M - j );
+                    answer /= ( j + 1 );
+                }
+                sb.AppendLine(answer.ToString());
+            }
+            Console.WriteLine(sb.ToString());
+        }
+    }
+}
