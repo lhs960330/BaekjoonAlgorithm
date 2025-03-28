c220253924b7748070773b9abe528033c4dc28cc
효석 이 <lhs19960330@gmail.com>
2025-03-28 16:06:17 +0900

    26069

---
+++ 26069/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace _26069
+{
+    internal class Program
+    {
+        // https://www.acmicpc.net/problem/26069
+
+        static void Main( string [] args )
+        {
+            int N = int.Parse(Console.ReadLine());
+            HashSet<string> metChongChong = new HashSet<string>();
+            for ( int i = 0; i < N; i++ )
+            {
+                string [] input = Console.ReadLine().Split().ToArray();
+                string A = input [0];
+                string B = input [1];
+                if ( A == "ChongChong" || B == "ChongChong" )
+                {
+                    metChongChong.Add(A);
+                    metChongChong.Add(B);
+                }
+                if ( metChongChong.Contains(A) )
+                {
+                    metChongChong.Add(B);
+                }
+                else if ( metChongChong.Contains(B))
+                {
+                    metChongChong.Add (A);
+                }
+            }
+            Console.WriteLine(metChongChong.Count);
+        }
+    }
+}
