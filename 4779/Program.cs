8210f262d9b9a9f7a3d3bee10e553e7c7f15dc14
효석 이 <lhs19960330@gmail.com>
2025-05-30 18:46:13 +0900

    feat : 4779

---
+++ 4779/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace _4779
+{
+    internal class Program
+    {
+        // https://www.acmicpc.net/problem/4779
+        static void Main( string [] args )
+        {
+            string line;
+            while ( ( line = Console.ReadLine() ) != null && line != "" )
+            {
+                int N = int.Parse(line);
+                int size = ( int )Math.Pow(3, N);
+                char [] result = new char [size];
+                for ( int i = 0; i < size; i++ )
+                    result [i] = '-';
+
+                Cantor(result, 0, size);
+
+                Console.WriteLine(new string(result));
+            }
+            static void Cantor( char [] arr, int start, int Lenght )
+            {
+                if ( Lenght < 3 )
+                    return;
+
+                int third = Lenght / 3;
+                for ( int i = start + third; i < start + 2 * third; i++ )
+                    arr [i] = ' ';
+
+                Cantor(arr, start, third);
+                Cantor(arr, start + 2 * third, third);
+            }
+        }
+    }
+}
